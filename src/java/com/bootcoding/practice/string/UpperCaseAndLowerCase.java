010171c2949d2fa9ba4c803edfc87e7266076598
prachihirekha <prachihirekhan32@gmail.com>
2023-05-05 13:02:39 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/string/UpperCaseAndLowerCase.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.string;
+
+public class UpperCaseAndLowerCase {
+    public static void main(String [] args){
+        String txt ="HeLLo WorLD";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+}
+
