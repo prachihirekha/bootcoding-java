3ac2f7a39c239fbcdaa37eae39704280fd44e202
prachihirekha <prachihirekhan32@gmail.com>
2023-05-05 13:12:19 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/string/Concatenation.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.string;
+
+public class Concatenation {
+    public static void main(String[] args){
+        String firstName =  "Ram";
+        String lastName =  "Shahu";
+        System.out.println(firstName+ " " +lastName);
+    }
+}
+
+
