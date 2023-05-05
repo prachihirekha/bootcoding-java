b12fafd66662dbe70d05184b8f40e30207108ee0
prachihirekha <prachihirekhan32@gmail.com>
2023-05-05 13:13:15 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/string/IndexOf.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.string;
+
+public class IndexOf {
+    public static void main(String[] args){
+        String word = "Give mi mu pen you are take this";
+        System.out.println(word.indexOf("Give"));
+    }
+}
+
