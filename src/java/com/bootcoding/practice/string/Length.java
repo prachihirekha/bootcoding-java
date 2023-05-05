d9568260671d4b0ee234a1d3783a9138738d797f
prachihirekha <prachihirekhan32@gmail.com>
2023-05-05 13:02:00 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/string/Length.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.string;
+
+public class Length {
+    public static void main(String [] args){
+        String t ="tjnfrghngwjjenvmasdljruibvdiurwf";
+        System.out.println("The length of variable is " + t.length());
+    }
+}
+
