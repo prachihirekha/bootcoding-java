95abd2fee2a40ea6a741c55b8cafad06f2c186e3
prachihirekha <prachihirekhan32@gmail.com>
2023-05-16 16:27:49 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/loop/Switchs.java
@@ -0,0 +1,32 @@
+package com.bootcoding.practice.loop;
+
+public class Switchs {
+    public static void main(String [] args){
+        int day =9;
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+            default :
+                System.out.println("There is no weekend");
+
+        }
+    }
+}
