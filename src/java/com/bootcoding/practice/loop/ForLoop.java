0d0c93e4ebca7ea65ace40cb45edfaa84eaa8dad
prachihirekha <prachihirekhan32@gmail.com>
2023-05-16 16:18:54 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/loop/ForLoop.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.loop;
+
+public class ForLoop {
+    public static void main(String[] agrs){
+        for(int i=0; i<6;i++){
+            System.out.println(i);
+        }
+    }
+}
+
