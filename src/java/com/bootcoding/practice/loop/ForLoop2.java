1df131695e8520283113ad5fa0e550aed070cf68
prachihirekha <prachihirekhan32@gmail.com>
2023-05-17 12:15:15 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/loop/ForLoop2.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.loop;
+
+public class ForLoop2 {
+
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "Bmw", "Ford"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
