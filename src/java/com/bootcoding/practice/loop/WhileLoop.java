29b601a69a05792c058238214b929ef829ab9cd0
prachihirekha <prachihirekhan32@gmail.com>
2023-05-17 12:16:10 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/loop/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.bootcoding.practice.loop;
+
+public class WhileLoop {
+    public static void main(String args[]){
+        int j=0;
+        do{
+            System.out.println(j);
+            j++;
+        }
+        while(j<5);
+
+    }
+}
