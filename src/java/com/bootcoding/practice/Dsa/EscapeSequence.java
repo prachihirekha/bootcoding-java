453e861422f582ef74a3b3fc06b6050748b2a4f2
prachihirekha <prachihirekhan32@gmail.com>
2023-05-21 16:15:35 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/Dsa/EscapeSequence.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.Dsa;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("I'"+"m alright");
+        System.out.println("I am software enginer in\"google\"");
+        System.out.println("Today is 14\\\\04\\\\2023");
+    }
+}
