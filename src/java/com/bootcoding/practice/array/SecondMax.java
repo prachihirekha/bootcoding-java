72674ae5d7f9d35cd59f27405dba2f2ec7c6b5cc
prachihirekha <prachihirekhan32@gmail.com>
2023-05-22 12:28:38 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/array/SecondMax.java
@@ -0,0 +1,16 @@
+package com.bootcoding.practice.array;
+
+public class SecondMax {
+    public static void main(String[] args) {
+        int[] a = {5,19,10,22,33};
+        int max = a[0];
+        int prevmax = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (max<a[i]) {
+                prevmax =max;
+                max = a[i];
+            }
+        }
+        System.out.println(prevmax);
+    }
+}
