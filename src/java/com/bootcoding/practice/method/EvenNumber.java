a5e624d884fbe7d8b5f7726e7775e5893b8cedbf
prachihirekha <prachihirekhan32@gmail.com>
2023-05-23 10:29:14 +0530

    java practice

---
+++ src/java/com/bootcoding/practice/method/EvenNumber.java
@@ -0,0 +1,15 @@
+package com.bootcoding.practice.method;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        printEvenNumber();
+    }
+    public static void printEvenNumber(){
+        int n = 7;
+    if(n%2==0) {
+        System.out.println(n +" is a even number");
+    }else{
+        System.out.println(n +"  is not a even number");
+    }
+    }
+}
