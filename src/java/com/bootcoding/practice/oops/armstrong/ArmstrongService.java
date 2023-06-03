dc9f8b0836a5cbed02d9904ba66859213772bd61
prachihirekha <prachihirekhan32@gmail.com>
2023-06-03 11:16:11 +0530

    practice

--- src/java/com/bootcoding/practice/oops/armstrong/ArmstrongService.java
+++ src/java/com/bootcoding/practice/oops/armstrong/ArmstrongService.java
@@ -1,27 +1,33 @@
 package com.bootcoding.practice.oops.armstrong;
 
 public class ArmstrongService {
-    public int armstrongService(int num ){
-       int rem =0;
-       int sum =0;
-       int temp =num;
-       while (num!=0){
-           rem =num%10;
-           sum =sum +(rem*rem*rem);
-           num =num/10;
-       }
-       if(sum ==temp){
-          return 1;
+    public boolean armstrongService(int num) {
+        int rem = 0;
+        int sum = 0;
+        int temp = num;
+        while (num != 0) {
+            rem = num % 10;
+            sum = sum + (rem * rem * rem);
+            num = num / 10;
+        }
+        if (sum == temp) {
+            return true;
 
 
-       }
-       return 0;
+        }
+        return false;
     }
-   public int[] multipleArmstrong(int[] arr){
-        int[] res =new int[arr.length];
-        for(int i=0 ;i<arr.length;i++){
-            res[i]=armstrongService(arr[i]);
+
+    public int[] multipleArmstrong(int[] arr) {
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            boolean armstrong =armstrongService(arr[i]);
+            if (armstrong) {
+                res[i] = arr[i];
+            }
+
         }
         return res;
     }
 }
+
