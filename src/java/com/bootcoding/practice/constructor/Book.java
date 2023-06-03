9fdb2e247cdbb9a8827468fcca9844eab98d44ba
prachihirekha <prachihirekhan32@gmail.com>
2023-06-03 10:53:16 +0530

    practice

---
+++ src/java/com/bootcoding/practice/constructor/Book.java
@@ -0,0 +1,8 @@
+package com.bootcoding.practice.constructor;
+
+public class Book {
+ String title;
+         String publisher;
+         double price;
+         Author author; // HAS a relation
+         }
