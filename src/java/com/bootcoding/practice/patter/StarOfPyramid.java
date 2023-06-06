079368e6f7110843a3ad1e0382ffd2c83bbc59fb
prachihirekha <prachihirekhan32@gmail.com>
2023-06-06 12:56:43 +0530

    practice

---
+++ src/java/com/bootcoding/practice/patter/StarOfPyramid.java
@@ -0,0 +1,19 @@
+package com.bootcoding.practice.patter;
+
+public class StarOfPyramid {
+    public static void main(String[] args) {
+        for(int i =1;i<=4;i++){
+            for (int j =4;j>=1;j--){
+                if(j>i) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+}
