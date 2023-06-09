e76cde54100c992a2df56e50e2999a38fa2c42a1
prachihirekha <prachihirekhan32@gmail.com>
2023-06-09 10:30:46 +0530

    practice

---
+++ src/java/com/bootcoding/practice/patter/SquareOfStar.java
@@ -0,0 +1,15 @@
+package com.bootcoding.practice.patter;
+
+public class SquareOfStar {
+    public static void main(String[] args) {
+        int n =4;
+        for(int i=1;i<=n;i++){
+            System.out.print(" ");
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
