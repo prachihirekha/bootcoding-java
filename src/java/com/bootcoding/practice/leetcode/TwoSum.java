ef694e298205e5522dbea0ce182871b3bec6a6c3
prachihirekha <prachihirekhan32@gmail.com>
2023-06-09 10:30:03 +0530

    practice

---
+++ src/java/com/bootcoding/practice/leetcode/TwoSum.java
@@ -0,0 +1,12 @@
+package com.bootcoding.practice.leetcode;
+
+//public class TwoSum {
+//    public static void main(String[] args) {
+//            TwoSum t =new TwoSum();
+//            int [] nums ={2,7,1,15};
+//            t.twoSum(nums);
+//    }
+//    public  int[] twoSum(int[] nums, int target){
+//
+//    }
+//}
