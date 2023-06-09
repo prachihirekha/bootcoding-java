fcc41c70e414d3fbbf1f490f75359e5c7f525d75
prachihirekha <prachihirekhan32@gmail.com>
2023-06-09 10:29:44 +0530

    practice

--- src/java/com/bootcoding/practice/array/ArrayMaximumNumber.java
+++ src/java/com/bootcoding/practice/array/ArrayMaximumNumber.java
@@ -2,11 +2,14 @@ package com.bootcoding.practice.array;
 //WAP to find max element of an array
 public class ArrayMaximumNumber {
     public static void main(String[] args) {
-        int [] arr ={10,20,40,20};
+        int [] arr ={10,30,40,20};
         int max=arr[0];
+      // int prevmax=0;
         for(int i=0;i<arr.length;i++){
             if (max < arr[i]) {
+                //prevmax=max;
                 max=arr[i];
+
             }
         }
 
