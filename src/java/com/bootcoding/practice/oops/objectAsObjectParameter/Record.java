ae41219e2ae2f624a5fa1ea14e35acc3ce56b244
prachihirekha <prachihirekhan32@gmail.com>
2023-06-10 12:02:10 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/objectAsObjectParameter/Record.java
@@ -0,0 +1,13 @@
+package com.bootcoding.practice.oops.objectAsObjectParameter;
+
+public class Record {
+    int[] arr;
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+}
