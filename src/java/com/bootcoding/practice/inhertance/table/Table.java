b07bd0a75e2a2333189ae66ec58feeea6b535188
prachihirekha <prachihirekhan32@gmail.com>
2023-07-11 15:27:11 +0530

    practice

---
+++ src/java/com/bootcoding/practice/inhertance/table/Table.java
@@ -0,0 +1,22 @@
+package com.bootcoding.practice.inhertance.table;
+
+public class Table {
+    private int legs;
+    private  String color;
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
