ce40d1bd8bb51e317c2efdaa532adc0bc106f0da
prachihirekha <prachihirekhan32@gmail.com>
2023-07-14 12:30:54 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/chocolate/book/AudioBook.java
@@ -0,0 +1,8 @@
+package com.bootcoding.practice.abstraction.chocolate.book;
+
+public class AudioBook extends Book{
+    @Override
+    void purchase(Double amount) {
+        title ="Atomic Habits";
+    }
+}
