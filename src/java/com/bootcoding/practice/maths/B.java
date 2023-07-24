ec4c511e551d03c96671a0fefe715047b4454cf7
prachihirekha <prachihirekhan32@gmail.com>
2023-07-24 10:40:52 +0530

    practice

---
+++ src/java/com/bootcoding/practice/maths/B.java
@@ -0,0 +1,13 @@
+//package com.bootcoding.practice.maths;
+//
+//public class B extends A{
+//    private int x;
+//    public B(){
+//        super.x =30;
+//        x=20;
+//
+//    }
+//    public int getX(){
+//        return x;
+//    }
+//}
