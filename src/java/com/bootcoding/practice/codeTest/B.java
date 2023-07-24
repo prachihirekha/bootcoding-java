2f8ef6876d43d824d6c3c5ea322b9559bf3b445e
prachihirekha <prachihirekhan32@gmail.com>
2023-07-24 10:40:00 +0530

    practice

---
+++ src/java/com/bootcoding/practice/codeTest/B.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.codeTest;
+
+public class B extends A{
+    @Override
+    public void print() {
+        System.out.println();
+        super.print();
+        System.out.println("B");
+    }
+}
