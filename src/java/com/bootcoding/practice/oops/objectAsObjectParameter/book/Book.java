384ee5f1e3acaa6bd39ad9c126efc5ec26fea090
prachihirekha <prachihirekhan32@gmail.com>
2023-07-26 15:28:19 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/objectAsObjectParameter/book/Book.java
@@ -0,0 +1,41 @@
+package com.bootcoding.practice.oops.objectAsObjectParameter.book;
+
+public class Book {
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    double price;
+    String bookName;
+    String authorName;
+    int numberOfPages;
+
+}
