a39671d4aa8e6e70cfbd35652c435dd042d05dff
prachihirekha <prachihirekhan32@gmail.com>
2023-08-01 16:16:46 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/usinginterface/loan/CarLoan.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.abstraction.usinginterface.loan;
+
+public class CarLoan implements Loan{
+    @Override
+    public void purchase(String name, double rate) {
+        System.out.println(name);
+        System.out.println(rate);
+    }
+}
+
