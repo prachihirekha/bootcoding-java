27023c1ce75249df00e4194f467a7971d44ac57a
prachihirekha <prachihirekhan32@gmail.com>
2023-08-01 16:17:49 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/chocolate/abstraction/Child3.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.abstraction.chocolate.abstraction;
+
+public class Child3 extends Parent {
+    void purchase(double amount)
+    {
+
+        message ="I am brought Daily milk";
+    }
+}
