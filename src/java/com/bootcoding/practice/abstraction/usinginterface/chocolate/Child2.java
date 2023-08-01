051ed9c148b2426a4ba33cc9b5b84d1123e740c4
prachihirekha <prachihirekhan32@gmail.com>
2023-08-01 16:17:19 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/usinginterface/chocolate/Child2.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.abstraction.usinginterface.chocolate;
+
+public class Child2 implements IParent{
+
+    public void purchase(double amount) {
+        System.out.println("I am child2 : I bought compass ");
+
+    }
+}
