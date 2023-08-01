2d7e3e22ceb2e963e78e7d4e8642258d7744d57e
prachihirekha <prachihirekhan32@gmail.com>
2023-08-01 16:17:02 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/chocolate/abstraction/Child2.java
@@ -0,0 +1,8 @@
+package com.bootcoding.practice.abstraction.chocolate.abstraction;
+
+public class Child2 extends Parent {
+    void purchase(double amount) {
+
+        message="I am brought lays";
+    }
+}
