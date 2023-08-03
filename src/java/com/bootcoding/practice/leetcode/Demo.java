85a344a772baa45c6e5961601df9e31e1ef6fdf0
prachihirekha <prachihirekhan32@gmail.com>
2023-08-03 17:04:48 +0530

    practice

---
+++ src/java/com/bootcoding/practice/leetcode/Demo.java
@@ -0,0 +1,14 @@
+package com.bootcoding.practice.leetcode;
+
+public class Demo {
+    public static void main(String[] args) {
+        String[]s ={"Let's take LeetCode contest"};
+
+        for(int i =s.length-1;i>=0;i--){
+
+            String v = s[i];
+            System.out.println(v);
+        }
+
+    }
+}
