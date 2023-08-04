de736c5c9ca1043b7b95a2fc72cf03cf3347a85e
prachihirekha <prachihirekhan32@gmail.com>
2023-08-04 20:56:42 +0530

    practice

---
+++ src/java/com/bootcoding/practice/abstraction/chocolate/book/EBook.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.abstraction.chocolate.book;
+
+public class EBook extends Book{
+    @Override
+    void purchase(Double amount) {
+        title= "Happy camper EBook";
+
+    }
+}
