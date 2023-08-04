f728b89661c08ddf6dd7b91a557c68cdb2b318ac
prachihirekha <prachihirekhan32@gmail.com>
2023-08-04 20:55:47 +0530

    practice

---
+++ src/java/com/bootcoding/practice/inhertance/Demo1.java
@@ -0,0 +1,11 @@
+//package com.bootcoding.practice.inhertance;
+//
+//public class Demo1 {
+//    public static void main(String[] args)
+//    {
+//        B b1 =new B();
+//        A a1 =b1;
+//        b1.insert();
+//    }
+//
+//}
