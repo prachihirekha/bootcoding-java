89e3e968633b6e3aa78bc854252b84e9a85f0029
prachihirekha <prachihirekhan32@gmail.com>
2023-08-13 14:27:23 +0530

    practice

---
+++ src/java/com/bootcoding/practice/Dsa/Find.java
@@ -0,0 +1,18 @@
+package com.bootcoding.practice.Dsa;
+
+public class Find {
+    public static void main(String[] args) {
+        String s ="rollNo"+","+"CSE"+","+"M1"+","+"M2"+","+"M3"+",";
+        StringBuilder sb =new StringBuilder();
+        sb.append("rollNo");
+        sb.append(",");
+        sb.append("CSE");
+        sb.append(",");
+        sb.append("M1");
+        sb.append(",");
+        sb.append("M2");
+        sb.append(",");
+        sb.append("M3");
+        System.out.println(s+sb);
+    }
+}
