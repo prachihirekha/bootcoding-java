78a9e0cdfb7d45e31b8a4190415bf6d4f7fb1bcf
prachihirekha <prachihirekhan32@gmail.com>
2023-08-13 14:26:55 +0530

    practice

---
+++ src/java/com/bootcoding/practice/cla/Festival.java
@@ -0,0 +1,11 @@
+//package com.bootcoding.practice.cla;
+//
+//public class Festival {
+//    public static void main(String[] args) {
+//        String s1 ="DIWALI";
+//        String s2 ="diwali";
+//        boolean res =s1.equals(s2);
+//        String s3 ="DIWALI";
+//        String s4 ="";
+//    }
+//}
