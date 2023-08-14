ce9ab0457074a3791c101266b08e16ea9ce60db0
prachihirekha <prachihirekhan32@gmail.com>
2023-08-14 11:26:35 +0530

    practice

---
+++ src/java/com/bootcoding/practice/cla/Foods.java
@@ -0,0 +1,13 @@
+package com.bootcoding.practice.cla;
+
+public class Foods {
+    public static void main(String[] args){
+        String s ="Haldiram";
+        int len =s.length();
+        String s2 ="Haldiram !";
+        System.out.println(s2.length());
+        String s3 ="!Haldiram   ";
+        System.out.println(s3.length());
+    }
+}
+
