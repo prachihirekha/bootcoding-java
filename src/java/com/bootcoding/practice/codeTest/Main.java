5a177755e3ab92a50c26c028681ac62e1c2b9383
prachihirekha <prachihirekhan32@gmail.com>
2023-09-09 12:58:57 +0530

    practice

---
+++ src/java/com/bootcoding/practice/codeTest/Main.java
@@ -0,0 +1,8 @@
+package com.bootcoding.practice.codeTest;
+
+public class Main {
+    public static void main(String[] args) {
+        A objA =new C();
+        objA.print();
+    }
+}
---
+++ src/java/com/bootcoding/practice/maths/Main.java
@@ -0,0 +1,11 @@
+//package com.bootcoding.practice.maths;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//         B objB =new B();
+//        System.out.println(objB.getX());
+//        System.out.println(objB.x);
+//        System.out.println(objB.getX()+ objB.x
+//        );
+//    }
+//}
