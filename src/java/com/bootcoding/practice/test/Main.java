59bdfb52edc193ecb014f808b250688307cbedc4
prachihirekha <prachihirekhan32@gmail.com>
2023-09-09 12:59:16 +0530

    practice

---
+++ src/java/com/bootcoding/practice/test/Main.java
@@ -0,0 +1,8 @@
+package com.bootcoding.practice.test;
+
+public class Main {
+    public static void main(String[] args){
+        B obj =new B(5,10);
+        System.out.println(obj.getX()+" "+obj.getY());
+    }
+}
