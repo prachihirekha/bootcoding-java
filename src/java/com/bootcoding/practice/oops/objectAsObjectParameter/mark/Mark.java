ce410f3add3405a742ed0d6b2e2e94c6c8087900
prachihirekha <prachihirekhan32@gmail.com>
2023-09-11 14:47:04 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/objectAsObjectParameter/mark/Mark.java
@@ -0,0 +1,31 @@
+package com.bootcoding.practice.oops.objectAsObjectParameter.mark;
+
+public class Mark {
+    double totalMark;
+    double ObtainedMark;
+    double percentage;
+
+    public double getTotalMark() {
+        return totalMark;
+    }
+
+    public void setTotalMark(double totalMark) {
+        this.totalMark = totalMark;
+    }
+
+    public double getObtainedMark() {
+        return ObtainedMark;
+    }
+
+    public void setObtainedMark(double obtainedMark) {
+        ObtainedMark = obtainedMark;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}
