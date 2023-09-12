91532635688209e2ae87558e89f7be12b8a86aa3
prachihirekha <prachihirekhan32@gmail.com>
2023-09-12 11:52:33 +0530

    practice

---
+++ src/java/com/bootcoding/practice/method/MethodEx8.java
@@ -0,0 +1,9 @@
+package com.bootcoding.practice.method;
+
+public class MethodEx8 {
+    public static void main(String args[])
+    {
+        String str ="I lOVE MYSELF  ";
+        System.out.println(str.length());
+    }
+}
