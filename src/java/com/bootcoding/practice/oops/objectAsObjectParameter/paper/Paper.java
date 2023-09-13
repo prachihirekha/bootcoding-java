e4dbeb6d7d4d03c5711219fc52adfb51481e5fb3
prachihirekha <prachihirekhan32@gmail.com>
2023-09-13 14:37:22 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/objectAsObjectParameter/paper/Paper.java
@@ -0,0 +1,31 @@
+package com.bootcoding.practice.oops.objectAsObjectParameter.paper;
+
+public class Paper {
+    String color;
+    String type;
+    double price;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
