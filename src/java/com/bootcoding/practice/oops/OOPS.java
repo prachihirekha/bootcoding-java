600fa14a0b2ebd7001590fff91242a7bbda8a148
prachihirekha <prachihirekhan32@gmail.com>
2023-09-13 14:35:23 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/OOPS.java
@@ -0,0 +1,13 @@
+package com.bootcoding.practice.oops;
+
+public class OOPS {
+    public static void main(String[] args) {
+        //object create
+        Student student = new Student();
+        student.name="Prachi";
+        student.lastName ="Hirekhan";
+        student.age=10;
+        student.standard =4;
+        student.details();
+    }
+}
