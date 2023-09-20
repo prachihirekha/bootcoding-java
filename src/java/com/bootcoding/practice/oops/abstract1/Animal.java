da9fca6f604dc39d0978ea915656c5ec6380beca
prachihirekha <prachihirekhan32@gmail.com>
2023-09-20 17:29:17 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/abstract1/Animal.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.oops.abstract1;
+
+abstract class Animal {
+    // constuctor
+    Animal(){
+        System.out.println("Creating a new animal");
+
+    }
+
+    abstract void walk();
+}
---
+++ src/java/com/bootcoding/practice/oops/abstract1/interface1/Animal.java
@@ -0,0 +1,6 @@
+package com.bootcoding.practice.oops.abstract1.interface1;
+interface Animal {
+    // pure abstract
+    int eye =2;
+    void walk();
+}
