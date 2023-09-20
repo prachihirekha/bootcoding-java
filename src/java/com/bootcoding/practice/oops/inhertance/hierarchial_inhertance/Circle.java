230f06e8b6354bfc91fdd69836dc9b8cf46eca16
prachihirekha <prachihirekhan32@gmail.com>
2023-09-20 17:30:12 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/inhertance/hierarchial_inhertance/Circle.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.oops.inhertance.hierarchial_inhertance;
+
+public class Circle extends Shape{
+    public void area (int r){
+        System.out.println("area of circle :"+(3.14)*r*r);
+    }
+}
