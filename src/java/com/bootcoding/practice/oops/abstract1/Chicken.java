14844a27ab6f6d08245624897dd431d4598f7d4f
prachihirekha <prachihirekhan32@gmail.com>
2023-09-20 17:29:53 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/abstract1/Chicken.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.oops.abstract1;
+
+public class Chicken extends Animal {
+    Chicken(){
+        System.out.println("Created the chicken");
+    }
+    public void walk(){
+        System.out.println("run 2 legs");
+    }
+}
