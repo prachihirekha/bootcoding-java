248d2f55466d635c5b5ba4a494ced7f4539e1a50
prachihirekha <prachihirekhan32@gmail.com>
2023-09-21 16:41:04 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/objectAsObjectParameter/paper/PaperService.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.oops.objectAsObjectParameter.paper;
+
+public class PaperService {
+    public void paper(Paper p){
+
+        System.out.println(p.getPrice());
+        System.out.println(p.getColor());
+        System.out.println(p.type);
+    }
+}
