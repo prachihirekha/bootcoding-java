8651bd6728f9208697a737e74c14b05aa1e2f486
prachihirekha <prachihirekhan32@gmail.com>
2023-10-10 12:28:37 +0530

    practice

---
+++ src/java/com/bootcoding/practice/loop/Project1Loops.java
@@ -0,0 +1,20 @@
+package com.bootcoding.practice.loop;
+
+public class Project1Loops {
+    public static void main(String[] args) {
+//        for(int i =0;i<100;i++){
+//            System.out.println("hello world" );
+//        }
+//        for (int j =0;j<=10;j++)  {
+//            System.out.print(j+" ");
+//        }
+        // by using while loop
+//
+        // by using do while loop
+        int i =1;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i<11);
+    }
+}
