7eeed1ebc34a4a122dc6fafb2da9a996b57f22dc
prachihirekha <prachihirekhan32@gmail.com>
2023-10-28 11:25:35 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/inhertance/hierarchial_inhertance/Shape.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.oops.inhertance.hierarchial_inhertance;
+
+public class Shape {
+    public void area(){
+        System.out.println("Display area");
+    }
+}
---
+++ src/java/com/bootcoding/practice/oops/inhertance/multilevel_inhertance/Shape.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.oops.inhertance.multilevel_inhertance;
+
+public class Shape {
+    public void area(){
+        System.out.println("area");
+    }
+}
