9881dcb8efb329a9cb150002514b6aa6fe283937
prachihirekha <prachihirekhan32@gmail.com>
2023-10-28 11:25:53 +0530

    practice

---
+++ src/java/com/bootcoding/practice/multilevelinheritance/Sofa.java
@@ -0,0 +1,6 @@
+package com.bootcoding.practice.multilevelinheritance;
+
+public class Sofa extends Furniture{
+    String price;
+
+}
---
+++ src/java/com/bootcoding/practice/oops/inhertance/single_level_inhertance/Shape.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.oops.inhertance.single_level_inhertance;
+
+public class Shape {
+    public void area(){
+        System.out.println("display area");
+    }
+}
