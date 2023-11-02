6d23547d9fde2039ce3e7b2e6e1cb686ee31e9f2
prachihirekha <prachihirekhan32@gmail.com>
2023-11-02 11:21:38 +0530

    practice

---
+++ src/java/com/bootcoding/practice/leetcode/TreeNode.java
@@ -0,0 +1,21 @@
+package com.bootcoding.practice.leetcode;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    int max;
+    public TreeNode(int data)
+    {
+        this.data=data;
+    }
+
+    public TreeNode(int data, TreeNode left, TreeNode right , int max)
+    {
+        this.data=data;
+        this.left=left;
+        this.right=right;
+        this.max=max;
+    }
+
+}
---
+++ src/java/com/bootcoding/practice/multilevelinheritance/vehicle/Track.java
@@ -0,0 +1,4 @@
+package com.bootcoding.practice.multilevelinheritance.vehicle;
+
+public class Track extends Vehicle{
+}
