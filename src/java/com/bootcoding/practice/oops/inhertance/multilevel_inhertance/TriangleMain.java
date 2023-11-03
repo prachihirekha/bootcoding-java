ddc046248e613ed9ed8d04164e88b004b09b7ce7
prachihirekha <prachihirekhan32@gmail.com>
2023-11-03 11:10:16 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/inhertance/multilevel_inhertance/TriangleMain.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.oops.inhertance.multilevel_inhertance;
+
+public class TriangleMain {
+    public static void main(String[] args) {
+        EquilateralTriangle t =new EquilateralTriangle();
+        t.area(20,30);
+       int s=t.side = 20;
+       System.out.println(s);
+    }
+
+}
---
+++ src/java/com/bootcoding/practice/oops/inhertance/single_level_inhertance/Triangle.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.oops.inhertance.single_level_inhertance;
+
+public class Triangle extends Shape {
+
+    public void area(int height,int length) {
+       double ans = (height*length)*1/2;
+        System.out.println(ans);
+
+    }
+}
