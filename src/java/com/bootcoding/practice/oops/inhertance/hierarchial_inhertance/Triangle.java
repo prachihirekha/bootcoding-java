e64dc3d74b399df2bd1ddce1454441e08478b4df
prachihirekha <prachihirekhan32@gmail.com>
2023-11-03 11:09:53 +0530

    practice

---
+++ src/java/com/bootcoding/practice/oops/inhertance/hierarchial_inhertance/Triangle.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.oops.inhertance.hierarchial_inhertance;
+
+public class Triangle extends Shape{
+    public void area(int h,int b){
+        System.out.println("Triangle :"+(h*b)/2);
+    }
+}
---
+++ src/java/com/bootcoding/practice/oops/inhertance/multilevel_inhertance/Triangle.java
@@ -0,0 +1,10 @@
+package com.bootcoding.practice.oops.inhertance.multilevel_inhertance;
+
+public class Triangle extends Shape{
+    //@Override
+    public void area(int height,int base) {
+      int a =(height*base)/2;
+        System.out.println(a);
+
+    }
+}
