337f4d343475c8b2115bd113e7496716268c25e2
prachihirekha <prachihirekhan32@gmail.com>
2024-01-19 17:07:25 +0530

    basic core java

---
+++ src/java/com/bootcoding/practice/basic/core/java/PrimitiveDataType.java
@@ -0,0 +1,20 @@
+package com.bootcoding.practice.basic.core.java;
+
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        char a ='A';
+        byte v = 2;
+        short c =22;
+        int j=234;
+        float f =1.2345676f;
+        double d =2.35465778654d;
+        boolean g =true;
+        System.out.println("char ="+a);
+        System.out.println("byte ="+v);
+        System.out.println("short ="+c);
+        System.out.println("int ="+j);
+        System.out.println("float ="+f);
+        System.out.println("double ="+d);
+        System.out.println("boolean ="+g);
+    }
+}
