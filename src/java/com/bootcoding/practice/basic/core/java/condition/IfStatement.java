eb5ae63121e29fa6eaa0ab54276a32c486067c55
prachihirekha <prachihirekhan32@gmail.com>
2024-01-31 11:17:03 +0530

    basic core java

---
+++ src/java/com/bootcoding/practice/basic/core/java/condition/IfStatement.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.basic.core.java.condition;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int num =100;
+        if(num>0){
+            System.out.println("number is positive");
+        }
+        System.out.println("this condition is terminated");
+    }
+}
