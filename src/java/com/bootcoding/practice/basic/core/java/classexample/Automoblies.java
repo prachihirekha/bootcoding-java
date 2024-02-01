bdd38640bac0f987d5463f5ecceaf2336d79e4ba
prachihirekha <prachihirekhan32@gmail.com>
2024-02-01 18:35:40 +0530

    Automobiles

---
+++ src/java/com/bootcoding/practice/basic/core/java/classexample/Automoblies.java
@@ -0,0 +1,32 @@
+package com.bootcoding.practice.basic.core.java.classexample;
+
+public class Automoblies {
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    private  String fuel;
+    private  int year;
+    private String brand;
+
+}
