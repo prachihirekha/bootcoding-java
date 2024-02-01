23af9fa357a7cbb8ae6a7b974f2c6bfc0add87c3
prachihirekha <prachihirekhan32@gmail.com>
2024-02-01 18:35:11 +0530

    Array example

---
+++ src/java/com/bootcoding/practice/basic/core/java/array/ArrayEx2.java
@@ -0,0 +1,12 @@
+package com.bootcoding.practice.basic.core.java.array;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum of array ="+ sum);
+    }
+}
