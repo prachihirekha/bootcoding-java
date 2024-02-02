f265f8d87149e0e1a6f2de1020b7429744a20850
prachihirekha <prachihirekhan32@gmail.com>
2024-02-02 17:09:29 +0530

    BIke class example

---
+++ src/java/com/bootcoding/practice/basic/core/java/classexample/Bike.java
@@ -0,0 +1,11 @@
+package com.bootcoding.practice.basic.core.java.classexample;
+
+import com.bootcoding.practice.basic.core.java.object.vehical.Bikes;
+
+public class Bike extends Automoblies {
+    Bike(){
+        System.out.println("Bike has been created!");
+    } public void kick(){
+        System.out.println("Bikes has been started with kick !");
+    }
+}
