d1eef8fd1ba5ed89e1a114d094a47f66221575f0
prachihirekha <prachihirekhan32@gmail.com>
2024-02-05 17:15:22 +0530

    polymorphism

--- src/java/com/bootcoding/practice/basic/core/java/oops/abstrction/polymorphism/overriding/Audi.java
+++
@@ -1,7 +0,0 @@
-package com.bootcoding.practice.basic.core.java.oops.abstrction.polymorphism.overriding;
-
-public class Audi {
-    int topSpeed(){
-        return 450;
-    }
-}
---
+++ src/java/com/bootcoding/practice/basic/core/java/oops/polymorphism/overriding/Audi.java
@@ -0,0 +1,7 @@
+package com.bootcoding.practice.basic.core.java.oops.polymorphism.overriding;
+
+public class Audi {
+    int topSpeed(){
+        return 450;
+    }
+}
